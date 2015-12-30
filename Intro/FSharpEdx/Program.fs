4e312f7ca25574a1866d948b4c3ef42f1d7a3a09
Gleb Dolgich <glebd70@gmail.com>
2015-12-30 21:19:55 +0000

    Initial commit of intro project

---
+++ Intro/FSharpEdx/Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+
+let cylinderVolume r h = Math.PI * r * r * h
+
+[<EntryPoint>]
+let main argv =
+
+  printf "Enter cylinder radius: "
+  let r = Console.ReadLine()
+
+  printf "Enter cylinder height: "
+  let h = Console.ReadLine()
+
+  printfn "Cylinder volume is %f" (cylinderVolume (float r) (float h))
+  0
